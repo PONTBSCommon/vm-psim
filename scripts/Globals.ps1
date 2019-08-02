c7165ff4db6de46e8e8010138040d4772732709e
Timm Bangma <bangma@hp.com>
2019-08-02 13:12:04 -0400

    updated psim installer

--- scripts/Globals.ps1
+++ scripts/Globals.ps1
@@ -1,9 +1,9 @@
 #### INSIDE VM LOCATIONS ####
 $FirstRunRemoteSetupScript = 'C:\vagrant\scripts\FirstRunRemoteSetup.ps1'
 $SetupMarker = 'C:\Program Files\PrinterOn Corporation\initial-vagrant-setup-marker.txt'
+$InstallationLogfile = 'C:\\Program Files (x86)\\PrinterOn Corporation\\PrinterOn Server Install Manager\\Installation History.txt'
 
-
-#### STANDARD PATHS ####
+#### STANDARD VM-PSIM PATHS ####
 $PsimManagePath = "$PSScriptRoot\..\psim-manage"
 $VagrantVirtualboxProviderFolder = '.\.vagrant\machines\default\virtualbox\'
 $VagrantMachineRootFolder = "$PSScriptRoot\.."
@@ -12,4 +12,3 @@ $PSModulesPath = ($env:PSModulePath -split ';' | ? { $_ -like '*Documents\Window
 #### PSIM INSTALLER CONSTANTS ####
 $PsimInstaller = 'C:\\vagrant\\installer\\psim.exe'
 $LicenseFile = 'C:\\vagrant\\installer\\license.txt'
-$InstallationLogfile = 'C:\\Program Files (x86)\\PrinterOn Corporation\\PrinterOn Server Install Manager\\Installation History.txt'
