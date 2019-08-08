24babdb59b9ddff8f3babe5a42b3df1867b10cad
Timm Bangma <bangma@hp.com>
2019-08-08 14:17:54 -0400

    comment out unused cert code for now

--- scripts/FirstRunRemoteSetup.ps1
+++ scripts/FirstRunRemoteSetup.ps1
@@ -11,10 +11,11 @@ try {
   Write-Host -ForegroundColor Yellow "Copying In psim-manage module."
   Copy-Item -Recurse -Force /vagrant/scripts/psim-manage $env:USERPROFILE\Documents\WindowsPowerShell\Modules\psim-manage
 
-  Write-Host -ForegroundColor Yellow "Adding the mobile certificate."
-  Invoke-RestMethod -Method Get -Uri $ATCACertWebLocation -OutFile $MobileCertificateSaveLocation
-  Import-Certificate -CertStoreLocation Cert:\CurrentUser\CA -FilePath $MobileCertificateSaveLocation
-  Remove-Item $MobileCertificateSaveLocation
+  #### This code will be used for adding a mobile certificate. (Later) ####
+  # Write-Host -ForegroundColor Yellow "Adding the mobile certificate."
+  # Invoke-RestMethod -Method Get -Uri $ATCACertWebLocation -OutFile $MobileCertificateSaveLocation
+  # Import-Certificate -CertStoreLocation Cert:\CurrentUser\CA -FilePath $MobileCertificateSaveLocation
+  # Remove-Item $MobileCertificateSaveLocation
 
   Write-Host -ForegroundColor Yellow 'First time setup completed.'
   echo 'DO NOT REMOVE' > $SetupMarker
