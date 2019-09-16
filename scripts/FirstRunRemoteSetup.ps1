6c43912ea7bfdb4e91c92f9ace8899aee59ea578
Timm Bangma <bangma@hp.com>
2019-09-16 13:55:55 -0400

    made install log quiet.

--- scripts/FirstRunRemoteSetup.ps1
+++ scripts/FirstRunRemoteSetup.ps1
@@ -8,10 +8,10 @@ echo 'Add file that indicated this initial setup has been done before.'
 New-Item -Force $SetupMarker; echo 'DO NOT REMOVE' > $SetupMarker
 
 echo "Installing zip tools."
-choco install zip unzip
+choco install --no-progress zip unzip
 
 echo "Installing baretail log viewer."
-choco install baretail
+choco install --no-progress baretail
 
 echo "Installing MSSQL Management Studio."
-choco install sql-server-management-studio
+choco install --no-progress sql-server-management-studio
