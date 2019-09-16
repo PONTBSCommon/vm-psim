750bccc80aeb25f19c7c6d6c86e8adc95a066d11
Timm Bangma <bangma@hp.com>
2019-09-16 12:30:38 -0400

    removed old global variables

--- scripts/Globals.ps1
+++ scripts/Globals.ps1
@@ -1,5 +1,4 @@
 #### INSIDE VM LOCATIONS ####
-$FirstRunRemoteSetupScript = 'C:\vagrant\scripts\FirstRunRemoteSetup.ps1'
 $SetupMarker = 'C:\Program Files\PrinterOn Corporation\initial-vagrant-setup-marker.txt'
 $MobileCertificateSaveLocation = 'C:\Users\vagrant\Downloads\atca.cer'
 
@@ -8,13 +7,6 @@ $MobileCertificateSaveLocation = 'C:\Users\vagrant\Downloads\atca.cer'
 $InstallationLogfile = 'C:\\Program Files (x86)\\PrinterOn Corporation\\PrinterOn Server Install Manager\\Installation History.txt' 
 $PsimInstaller = 'C:\\vagrant\\installer\\psim.exe'
 $LicenseFile = 'C:\\vagrant\\installer\\license.txt'
-$FullInstallIndicator = 'C:\\vagrant\\installer\\full_install.txt'
-
-#### STANDARD VM-PSIM PATHS ####
-$PsimManagePath = "$PSScriptRoot\..\psim-manage"
-$VagrantVirtualboxProviderFolder = '.\.vagrant\machines\default\virtualbox\'
-$VagrantMachineRootFolder = "$PSScriptRoot\.."
-$PSModulesPath = ($env:PSModulePath -split ';' | ? { $_ -like '*Documents\WindowsPowerShell\Modules' })
 
 #### EXTERNAL RESOURCES ####
 $ATCACertWebLocation = 'http://webdepot.printeron.local/atca.cer'
