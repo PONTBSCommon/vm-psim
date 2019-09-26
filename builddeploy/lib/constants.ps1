5e1738a3e175471162ea4d9552fc3a80ceaec7dc
roberto-alves-santos <roberto.alves.santos@hp.com>
2019-09-26 16:30:07 -0400

    merge conflicts

--- builddeploy/lib/constants.ps1
+++ builddeploy/lib/constants.ps1
@@ -2,9 +2,7 @@
 
 # PSIM directories
 $TomcatWebappDir = "C:\Program Files (x86)\PrinterOn Corporation\Apache Tomcat\webapps"
-<<<<<<< HEAD
-$PDSDir = "C:\Program Files (x86)\PrinterOn Corporation\PDS\client"
-=======
+
 $PSIMParentDir = "C:\Program Files (x86)\PrinterOn Corporation\PrinterOn Server Install Manager"
 $PonConfPropertiesDir = "C:\Program Files (x86)\PrinterOn Corporation\PrinterOn Server Install Manager\printeron-web\WEB-INF\classes"
->>>>>>> 4e6106a6440c19610e5460f4e254f5f429307922
+$PDSDir = "C:\Program Files (x86)\PrinterOn Corporation\PDS\client"
